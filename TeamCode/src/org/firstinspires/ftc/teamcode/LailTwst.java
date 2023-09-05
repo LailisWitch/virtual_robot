c26ec6029b5c4a87e171332edbc29cb543ee0444
LailisWitch <143556032+LailisWitch@users.noreply.github.com>
2023-09-05 16:13:25 -0500

    Made Name Print

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/LailTwst.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "LailTwst")
+public class LailTwst extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("name", "Lail");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
+}
+
